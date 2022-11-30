aef6f0a8fa775f44625842605dd279cd01ee4c6c
Marco Busellato <marco.busellato@develondigital.com>
2022-11-30 13:57:35 +0100

    removed auth module, app controller and app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,9 +14,7 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true
     }),
     ReportsModule, 
-    UsersModule, AuthModule
+    UsersModule
   ],
-  // controllers: [AppController],
-  // providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthController } from '../auth/auth.controller';
+import { AuthService } from '../auth/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity'
 
@@ -8,7 +10,7 @@ import { User } from './user.entity'
   imports: [
     TypeOrmModule.forFeature([User])
   ],
-  controllers: [UsersController],
-  providers: [UsersService]
+  controllers: [UsersController, AuthController],
+  providers: [UsersService, AuthService]
 })
 export class UsersModule {}
