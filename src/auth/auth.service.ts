2ead0fefad1f6ec468cf864d95e49a207c2b6776
Marco Busellato <marco.busellato@develondigital.com>
2022-11-30 14:41:59 +0100

    added hashed password (bcrypt) to user signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,15 +13,16 @@ export class AuthService {
 
   async signup(body: CreateUserDto) {
     const { email, password } = body;
-
     const userExist = await this.getUserByEmail(email)
     if(userExist) {
       throw new ConflictException('L\'utente esiste già!')
     }
 
+    const hashedPassword = await bcrypt.hash(password, 12)
+
     const user = this.repo.create({
       email,
-      password
+      password: hashedPassword
     })
 
     return this.repo.save(user);
