620dabb2f7c31a5b7b41651c603552e91de6e3ec
Marco Busellato <marco.busellato@develondigital.com>
2022-12-06 14:46:43 +0100

    init unit test of users service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,50 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { UsersService } from './users.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { User } from './user.entity'
+import { BadRequestException } from '@nestjs/common'
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let service: UsersService
+  let repo: Repository<User>
+  const user = {
+    id: 10,
+    email: 'test@mail.com',
+    password: 'testpassword'
+  } as User
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findOneBy: ({ id }) => {
+              const response = user.id === id ?
+                user :
+                null
+                
+              return Promise.resolve(response)
+            }
+          }
+        }
+      ],
+    }).compile()
 
-    service = module.get<UsersService>(UsersService);
-  });
+    service = module.get<UsersService>(UsersService)
+    repo = module.get<Repository<User>>(getRepositoryToken(User))
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+
+  it('throws an error if user does not exist', async () => {
+    await expect(
+      service.updateUser(2, {})
+    ).rejects
+    .toBeInstanceOf(BadRequestException)
+  })
+})
